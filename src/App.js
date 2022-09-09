a0de7bd3f68ca9b488f68d9b5fac363856d93266
tadomotoji <thienluong675@gmail.com>
2022-09-09 17:35:15 +0700

    reactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+
+  
+  const [jobsList, setJobsList] = useState(() => {
+    const storageJobsList = JSON.parse(localStorage.getItem('jobsList'));
+    return storageJobsList ?? []
+  });
+  const [job, setJob] = useState('');
+
+
+  const handleAdd = () => {
+    setJobsList(prev => {
+      const newJobsList = [...prev, job];
+      const jsonJobsList = JSON.stringify(newJobsList);
+      localStorage.setItem('jobsList',jsonJobsList)
+      return newJobsList;
+    });
+    setJob('')
+  }
+
+  const handleDelete = (index) => {
+    setJobsList(prev => {
+      prev.splice(index,1);
+      //console.log(prev);
+      localStorage.setItem('jobsList',JSON.stringify(prev));
+      return prev
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {jobsList.map((job, index) => <li key={index}>
+          {job}
+           <button onClick={() => {handleDelete(index)}}>Delete</button>
+        </li>)}
+      </ul>
+      <input value={job} onChange={e => { setJob(e.target.value) }} />
+      <button onClick={handleAdd}>Add New Job</button>
+
     </div>
   );
 }
