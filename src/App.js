38f47a741c9501d4e66bc35f6bbc2944413b7523
tadomotoji <thienluong675@gmail.com>
2022-09-13 17:23:55 +0700

    todolist v1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useState, useRef, useEffect } from 'react'
-
+import Heading from './components/heading'
 function App() {
   const addRef = useRef();
-
-
+ 
   const [jobsList, setJobsList] = useState(() => {
     const storageJobsList = JSON.parse(localStorage.getItem('jobsList'));
     return storageJobsList ?? []
   });
+
+  const [currentIndex,setCurrentIndex] = useState(null);
+  const [currentJob, setCurrentJob] = useState('');
   const [job, setJob] = useState('');
 
 
@@ -28,9 +30,26 @@ function App() {
     })
   }
   const handleEdit = (index) => {
-
+    if (currentIndex !== index)  
+    {
+      setCurrentIndex(index);
+      setCurrentJob(jobsList[index])
+    }  
+    else
+    {
+      setCurrentIndex(null);
+      setJobsList(prev => {
+        const arr = [...prev];
+        arr[index]=currentJob;
+        return arr
+      });
+      setCurrentJob('');
+    }
   }
 
+  
+
+  
   //update local storage
   useEffect(() => {
     const jsonJobsList = JSON.stringify(jobsList);
@@ -40,12 +59,15 @@ function App() {
 
   return (
     <div className="App">
+      <Heading/>
       <ul>
         {jobsList.map((job, index) =>
           <li key={index}>
-            {job}
-            <button onClick={() => { handleEdit(index) }}>Edit</button>
-            <button onClick={() => { handleDelete(index) }}>Delete</button>
+            {index === currentIndex ? <input autoFocus placeholder={job}
+            onChange = {e => { setCurrentJob(e.target.value) }}
+            ></input> : <span onClick={() => { handleEdit(index) }}>{job}</span>}
+            {index === currentIndex ? <button onClick={() => { handleEdit(index) }}>Update</button> :
+            <button onClick={() => { handleDelete(index) }}>Delete</button>}
           </li>)}
       </ul>
       <input ref={addRef} value={job} placeholder="add todo job" onChange=
---
+++ src/components/heading/index.js
@@ -0,0 +1,9 @@
+import "./heading.css"
+
+function Heading(){
+    return(
+        <h1 className="heading">Todo List</h1>
+    )
+}
+
+export default Heading
